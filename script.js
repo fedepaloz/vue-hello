3dddfaa4d61f9605f752b7ecb8dd00f5aa7374ec
fedepaloz <palozzifederico@gmail.com>
2022-07-11 15:51:32 +0200

    esercizio finito

---
+++ script.js
@@ -0,0 +1,12 @@
+const root = new Vue ({
+    el : '#root' ,
+    data : {
+        message: 'piacere sono Federico Palozzi',
+        img: 'https://i.pinimg.com/280x280_RS/aa/68/3c/aa683cfec8bc77aee86f21dde53464a3.jpg'
+    },
+    methods : {},
+})
+
+
+
+console.log("ciao", Vue)
